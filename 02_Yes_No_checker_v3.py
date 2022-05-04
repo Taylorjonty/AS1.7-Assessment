34fd2b7f18824a3185afd967ed37012272bceb59
Taylorjonty <98923451+Taylorjonty@users.noreply.github.com>
2022-05-04 14:17:17 +1200

    Update 02_Yes_No_checker_v3.py

--- 02_Yes_No_checker_v3.py
+++ 02_Yes_No_checker_v3.py
@@ -1,20 +1,24 @@
-# Similar to
+# Similar to 02_yes_no_checker_v2
+# turned it into a function to copy to the base code
+
 def yes_no(question_text):
     while True:
-
+        # Ask if the person has played before
         answer = input(question_text).lower()
-
+        # If the answer is yes, continue
         if answer == "yes" or answer == "y":
             answer = "Yes"
+            print("continue code")
             return answer
-
+        # If the answer is no, show the instructions
         elif answer == "no" or answer == "n":
             answer = "No"
+            print("instructions")
             return answer
-
+        # Tell player to answer yes or no
         else:
             print("Please answer 'yes' or 'no'")
 
 
 show_instruction = yes_no("Have you played this game before: ")
-print(f"You entered {show_instruction}")
+
