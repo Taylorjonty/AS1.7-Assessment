cf07edc92ec38750c7309857a9dfd49489f49725
Taylorjonty <98923451+Taylorjonty@users.noreply.github.com>
2022-05-04 13:59:37 +1200

    welcome screen 1

---
+++ 01_Welcome_screen_v1.py
@@ -0,0 +1,12 @@
+# This will be the very start of the game
+user_name = ""
+# Welcome the player to the game
+print("Welcome to the maori language quiz!")
+print()
+# Ask what the players name is
+user_name = input("What is your name: ")
+print()
+# Say hello to the player
+print(f"Hello {user_name}")
+
+
