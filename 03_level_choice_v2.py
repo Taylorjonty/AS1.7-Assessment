d86793c22221a02d9ef2b51ab3bb97f0fadb8c4c
Taylorjonty <98923451+Taylorjonty@users.noreply.github.com>
2022-05-11 13:41:17 +1200

    Update 03_level_choice_v2.py

--- 03_level_choice_v2.py
+++ 03_level_choice_v2.py
@@ -4,15 +4,21 @@
 choice = ""
 l_choice = ""
 
+# Asks whether the player wants to play easy or hard mode (part 1)
 print("Would you like easy or hard mode?")
+# While there is no answer entered repeat the loop
 while l_choice != "e" or l_choice != "h":
+    # Asks whether the player wants to play easy or hard mode (part 2)
     l_choice = input("Press E for easy mode. Press H for hard mode: ").lower()
+    # if the player answer is easy play this part
     if l_choice == "e":
         choice = "Easy"
         break
+    # if the player answer is hard play this part
     elif l_choice == "h":
         choice = "Hard"
         break
+    # if the player answer is not either, redo loop
     else:
         print("please choose easy or hard by pressing E or H")
 
