00f3b0d38df90cbd6b6a77d031d2103e1caa5ab8
Taylorjonty <98923451+Taylorjonty@users.noreply.github.com>
2022-05-17 08:47:56 +1200

    Create 03_level_choice_v3.py

---
+++ 03_level_choice_v3.py
@@ -0,0 +1,16 @@
+# this component finds out if the player wants to play easy mode or hard mode. it has been turned into a function
+# based on 03_level_choice_v1
+def ask_level():
+    l_choice_local = input("What difficulty level would you like.\n""\nPress E for easy or H for hard: ").lower()
+    if l_choice_local == "e":
+        l_choice_local = "easy"
+    else:
+        l_choice_local = "hard"
+
+    return l_choice_local
+
+
+# main routine
+l_choice_local = ask_level()
+print(f"{l_choice_local}")
+
